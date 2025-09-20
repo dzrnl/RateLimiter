6ddd243cbb1774f7f0a158085a6f95d964bcb1fa
Danil Salnikov <dzrnl@outlook.com>
2025-09-20 23:20:01 +0300

    fix: UserMapper.ToUpdateModel

--- UserService/src/Controllers/UserMapper.cs
+++ UserService/src/Controllers/UserMapper.cs
@@ -7,10 +7,19 @@ namespace UserService.Controllers;
 [Mapper]
 public partial class UserMapper
 {
-    [MapperIgnoreSource(nameof(userDomain.Password))]
-    public partial UserResponse FromModel(UserModel userDomain);
+    [MapperIgnoreSource(nameof(UserModel.Password))]
+    public partial UserResponse FromModel(UserModel model);
 
     public partial CreateUserDto ToCreateModel(CreateUserRequest request);
 
-    public partial UpdateUserDto ToUpdateModel(UpdateUserRequest request); // TODO
+    public UpdateUserDto ToUpdateModel(UpdateUserRequest request)
+    {
+        return new UpdateUserDto(
+            request.Id,
+            request.HasPassword ? request.Password : null,
+            request.HasName ? request.Name : null,
+            request.HasSurname ? request.Surname : null,
+            request.HasAge ? request.Age : null
+        );
+    }
 }
