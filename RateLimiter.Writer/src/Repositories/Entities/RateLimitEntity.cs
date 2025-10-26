add8462fc221e8b5055b58d1f0d538083efe7f5f
Danil Salnikov <dzrnl@outlook.com>
2025-10-26 22:30:28 +0300

    style: use record for db models

--- RateLimiter.Writer/src/Repositories/Entities/RateLimitEntity.cs
+++ RateLimiter.Writer/src/Repositories/Entities/RateLimitEntity.cs
@@ -3,9 +3,4 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace RateLimiter.Writer.Repositories.Entities;
 
 [BsonIgnoreExtraElements]
-public class RateLimitEntity(string route, int requestsPerMinute)
-{
-    public string Route { get; set; } = route;
-
-    public int RequestsPerMinute { get; set; } = requestsPerMinute;
-}
+public record RateLimitEntity(string Route, int RequestsPerMinute);
