557a5aa9d92ff856469524fc94476f966d9eb733
Danil Salnikov <dzrnl@outlook.com>
2025-11-24 19:27:01 +0300

    refactor: make UserRequest record struct

--- UserRequestsKafkaGenerator/Models/UserRequest.cs
+++ UserRequestsKafkaGenerator/Models/UserRequest.cs
@@ -1,30 +1,3 @@
 namespace UserRequestsKafkaGenerator.Models;
 
-public readonly struct UserRequest : IEquatable<UserRequest>
-{
-    public int UserId { get; }
-    public string Endpoint { get; }
-
-    public UserRequest(int userId, string endpoint)
-    {
-        UserId = userId;
-        Endpoint = endpoint;
-    }
-
-    public bool Equals(UserRequest other)
-    {
-        return UserId == other.UserId && string.Equals(Endpoint, other.Endpoint, StringComparison.Ordinal);
-    }
-
-    public override bool Equals(object? obj)
-        => obj is UserRequest other && Equals(other);
-
-    public override int GetHashCode()
-        => HashCode.Combine(UserId, Endpoint);
-
-    public static bool operator ==(UserRequest left, UserRequest right)
-        => left.Equals(right);
-
-    public static bool operator !=(UserRequest left, UserRequest right)
-        => !(left == right);
-}
+public readonly record struct UserRequest(int UserId, string Endpoint);
