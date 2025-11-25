d2813ac62f1d58f25d719cc3b205175899d2da3e
KovrikAlexandr <abc@>
2025-11-25 15:21:07 +0300

    fix(mongo): replset initiate

--- RateLimiter.Writer/database/mongo-init.bash
+++ RateLimiter.Writer/database/mongo-init.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongod --replSet rs0 --bind_ip_all &
+mongod --replSet rs0 --bind_ip_all >/dev/null 2>&1 &
 
 RETRY_DELAY=3
 until mongosh --quiet --eval "res = db.adminCommand({ ping: 1 }); quit(res.ok ? 0 : 1)" >/dev/null 2>&1; do
@@ -8,12 +8,9 @@ until mongosh --quiet --eval "res = db.adminCommand({ ping: 1 }); quit(res.ok ? 
   sleep "${RETRY_DELAY}"
 done
 
-mongosh --quiet --eval "
-  if (!rs.status().ok) { 
-    print('Initializing replica set...'); 
-    rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'limits-db:27017' }] }); 
-  } else { 
-    print('Replica set already initialized.'); 
-  }"
+if ! mongosh --quiet --eval "rs.status()" >/dev/null 2>&1; then
+  mongosh --quiet --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'limits-db:27017' }] });"
+  echo "Replica set initiated"
+fi
 
 wait
