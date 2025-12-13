2fbb8439faac32ba60764a054438334814ddd4ba
Danil Salnikov <dzrnl@outlook.com>
2025-12-13 18:40:52 +0300

    fix: remove route validation in writer

--- RateLimiter.Writer/src/Controllers/Validators/CreateRateLimitRequestValidator.cs
+++ RateLimiter.Writer/src/Controllers/Validators/CreateRateLimitRequestValidator.cs
@@ -6,11 +6,6 @@ public class CreateRateLimitRequestValidator : AbstractValidator<CreateRateLimit
 {
     public CreateRateLimitRequestValidator()
     {
-        RuleFor(x => x.Route)
-            .NotEmpty().WithMessage("Route is required")
-            .Matches(@"^[a-zA-Z_][a-zA-Z0-9_]*(\.[a-zA-Z_][a-zA-Z0-9_]*)+$")
-            .WithMessage("Route must be a valid gRPC method name. E.g. ServiceName.MethodName or namespace.Service.Method");
-
         RuleFor(x => x.RequestsPerMinute)
             .GreaterThan(0).WithMessage("Requests per minute must be greater than 0");
     }
